495e3c25dc53241391c02ce6203fdd0703d4a2f8
Joseph Paul <joepubacc@gmail.com>
2023-10-13 22:51:20 -0400

    initialize eslint

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-// this file makes a port on my local machine listen for get requests on port 3000 and responds to the requests
+// this file makes a port on my local machine listen for get
+// requests on port 3000 and responds to the requests
 // with the file at ./client/index.html
 
 // first, I need to get the built in modules from node
@@ -8,6 +9,7 @@ const path = require('path');
 
 // get express and create an app
 const express = require('express');
+
 const app = express();
 
 // set PORT
@@ -29,11 +31,13 @@ const requestListener = (req, res) => {
   // output: not sure it matters??
   // side-effects: send the response object to the client that sent the request object
 
-  // if the request is a get request for the base path that the server is listening on, serve the index.html
+  // if the request is a get request for the base path that
+  // the server is listening on, serve the index.html
   if (req.method === 'GET' && req.url === '/') {
     // write 200 to the response header and set the content type header to text/html
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    // use readFileSync to get the index.html data and pass it into the end method on the response object
+    // use readFileSync to get the index.html data and pass it
+    // into the end method on the response object
     return res.end(fs.readFileSync(path.join('.', 'client', 'index.html'), 'utf-8'));
   } else { // set up a 404 not found response for all others
     res.writeHead(404);
@@ -49,6 +53,6 @@ app.use((req, res) => {
 });
 
 // make the server listen on port 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Listening on port 3000.');
 });
